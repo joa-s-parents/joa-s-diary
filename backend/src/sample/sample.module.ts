bfddd807e268d996bd606c9b4a32bf1b94a72b41
junki-dev <kjgcde@gmail.com>
2022-12-18 18:16:31 +0900

    chore: 미사용 모듈 삭제

--- backend/src/sample/sample.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { SampleService } from '@sample/sample.service';
-import { SampleController } from '@sample/sample.controller';
-
-@Module({
-  controllers: [SampleController],
-  providers: [SampleService],
-})
-export class SampleModule {}
